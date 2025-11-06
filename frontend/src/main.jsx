55d3c3a0161a97b4dfbc85d167426c4676a89959
Harish Nigam <harrysingh17022552@gmail.com>
2025-11-06 16:42:28 +0530

    Component will be used and its routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import AddBook from "./component/AddBook.jsx";
+import BrowseBooks from "./component/BrowseBooks.jsx";
+import BookCategory from "./component/BookCategory";
+import BookDetails from "./component/BookDetails.jsx";
+const RootProvider = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "addbook", element: <AddBook /> },
+        { path: "browsebooks", element: <BrowseBooks /> },
+        { path: "bookcategory/:category", element: <BookCategory /> },
+        { path: "bookdetails/:id", element: <BookDetails /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RootProvider />
+  </StrictMode>
+);
